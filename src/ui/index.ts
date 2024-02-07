11cb05645d850c5707b533e03cf35cbedb30dc27
Karim Machlab <karim@vegascity.org>
2024-02-07 17:14:59 +0000

    time UI rename

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,12 +1,12 @@
 import { SpeedometerUI } from "./speedometerUI";
-import { LapUI } from "./lapUI";
+import { TimeUI } from "./timeUI";
 import { MinimapConfig, Minimap } from "./minimap";
 import { Dashboard } from "./dashboard";
 import { CarChoiceUI } from "./carChoiceUI";
 import { Countdown } from "./countdown";
 
 export { SpeedometerUI }
-export { LapUI }
+export { TimeUI }
 export { MinimapConfig, Minimap }
 export { Dashboard }
 export { CarChoiceUI }
