79e2311ad216a5a86dba98f229d81a4a7b8c61b3
Karim Machlab <karim@vegascity.org>
2024-03-04 16:45:04 +0000

    custom loading bar + car exit ui

--- src/ui/exitCarUI.tsx
+++ src/ui/exitCarUI.tsx
@@ -3,29 +3,75 @@ import { UiCanvasInformation, engine } from "@dcl/sdk/ecs"
 import { Color4 } from "@dcl/sdk/math"
 import { GameManager, InputManager, TrackManager } from "../racetrack"
 import { Car } from "../car"
-import * as ui from 'dcl-ui-toolkit'
 
 export class ExitCarUI {
     static initialised: boolean = false
     static visibility: boolean = false
-    static holdBar: ui.UIBar
     static opacity: number = 0
     static progress: number = 0
 
     private static component = () => (
         <UiEntity
             uiTransform={{
-                position: { right: ExitCarUI.getImagePosX(), bottom: ExitCarUI.getImagePosY() },
+                position: { right: ExitCarUI.getBarPosX(), bottom: ExitCarUI.getBarPosY() },
                 height: 100,
                 width: 100,
                 positionType: 'absolute',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                alignContent: 'center',
                 display: ExitCarUI.visibility ? 'flex' : 'none'
             }}
         >
             <UiEntity
                 uiTransform={{
                     width: 512 * 0.5,
-                    height: 256 * 0.5,
+                    height: 512 * 0.5,
+                    positionType: 'absolute',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    alignContent: 'center',
+                }}
+                uiBackground={{
+                    textureMode: 'stretch',
+                    texture: {
+                        src: "images/ui/loadingBarBkg.png",
+                        wrapMode: 'repeat'
+                    },
+                    color: Color4.create(1, 1, 1, ExitCarUI.opacity)
+                }}
+            >
+            </UiEntity>
+            <UiEntity
+                uiTransform={{
+                    position: { left: 3.2 - (2.7 * ExitCarUI.progress), bottom: -90 },
+                    width: 512 * 0.5 * ExitCarUI.progress,
+                    height: 512 * 0.55,
+                    positionType: 'absolute',
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    alignContent: 'center',
+                }}
+                uiBackground={{
+                    textureMode: 'nine-slices',
+                    texture: {
+                        src: "images/ui/loadingBarFill.png",
+                        wrapMode: 'repeat'
+                    },
+                    textureSlices: {
+                        top: 0,
+                        bottom: 0,
+                        left: ExitCarUI.progress < 0.2 ? (0.2 - ExitCarUI.progress) : 1,
+                        right: 1
+                    },
+                    color: Color4.create(1, 1, 1, ExitCarUI.opacity)
+                }}
+            >
+            </UiEntity>
+            <UiEntity
+                uiTransform={{
+                    width: 512 * 0.5,
+                    height: 512 * 0.5,
                     positionType: 'absolute',
                 }}
                 uiBackground={{
@@ -52,7 +98,6 @@ export class ExitCarUI {
             ExitCarUI.initialise()
         }
 
-        ExitCarUI.holdBar.show()
         ExitCarUI.visibility = true
     }
 
@@ -64,27 +109,14 @@ export class ExitCarUI {
         ExitCarUI.opacity = 0
         ExitCarUI.progress = 0
         ExitCarUI.visibility = false
-        ExitCarUI.holdBar.hide()
     }
 
     private static initialise(): void {
-        ExitCarUI.holdBar = ui.createComponent(ui.UIBar, {
-            value: 0,
-            xOffset: 0,
-            yOffset: 0,
-            color: Color4.White(),
-            style: ui.BarStyles.ROUNDBLACK,
-            scale: 1.8,
-        })
         engine.addSystem(ExitCarUI.update)
-
         ExitCarUI.initialised = true
     }
 
     private static update(_dt: number): void {
-        ExitCarUI.holdBar.xOffset = ExitCarUI.getBarPosX()
-        ExitCarUI.holdBar.yOffset = ExitCarUI.getBarPosY()
-
         if (ExitCarUI.visibility) {
             ExitCarUI.opacity += _dt * 0.5
             if (ExitCarUI.opacity > 1) {
@@ -108,28 +140,16 @@ export class ExitCarUI {
                     ExitCarUI.progress = 0
                 }
             }
-
-            ExitCarUI.holdBar.set(ExitCarUI.progress)
         }
-    }
-
-    private static getImagePosX(): number {
-        let canvas = UiCanvasInformation.get(engine.RootEntity)
-        return canvas.width * 0.51
-    }
-
-    private static getImagePosY(): number {
-        let canvas = UiCanvasInformation.get(engine.RootEntity)
-        return canvas.height * 0.08
     }
 
     private static getBarPosX(): number {
         let canvas = UiCanvasInformation.get(engine.RootEntity)
-        return (-canvas.width * 0.51) + 144
+        return canvas.width * 0.51
     }
 
     private static getBarPosY(): number {
         let canvas = UiCanvasInformation.get(engine.RootEntity)
-        return canvas.height * 0.03
+        return canvas.height * 0.01
     }
 }
